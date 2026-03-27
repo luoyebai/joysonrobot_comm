32cd6d00e4d5f6ed58d51e985d51ffb69e7c0fa1
luoyebai <2112216825@qq.com>
2026-03-27 15:47:06 +0800

    [refactor](scripts) rename environment variable in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-export JOYSON_ROBOT_BASE=/opt/joysonrobot
-export LD_LIBRARY_PATH=$JOYSON_ROBOT_BASE/lib/:$LD_LIBRARY_PATH
-
-
+export JOYSON_ROBOT_INSTALLED=/opt/joysonrobot
+export LD_LIBRARY_PATH=$JOYSON_ROBOT_INSTALLED/lib/:$LD_LIBRARY_PATH
